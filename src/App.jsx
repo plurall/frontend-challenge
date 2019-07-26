af5e896ea22129a79eb3e0796a63dc37fd532065
Giovanni Keppelen <keppelen.as@gmail.com>
2019-07-26 11:55:31 -0300

    resolve bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import React, { Component } from 'react'
-import { Switch } from 'react-router-dom'
+import React from 'react'
 
-import PlurallTracker from 'plurall-tracker'
-
-import { Layout, PrivateRoute } from 'components'
+import { Layout } from 'components'
 import { Home } from 'views'
 
 import './App.module.css'
 
-window.tracker = new PlurallTracker({
-  env: 'staging',
-  clientId: 'frontEndChallange',
-})
-
-class App extends Component {
-  async componentDidMount() {
-    window.tracker.pageView()
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Home />
-      </Layout>
-    )
-  }
-}
+const App = () => (
+  <Layout>
+    <Home />
+  </Layout>
+)
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import 'babel-polyfill'
 import 'react-app-polyfill/ie9'
 
-import { Router, Route, Switch } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -11,11 +10,8 @@ import { PrivateRoute } from 'components'
 import App from 'App'
 import registerServiceWorker from 'registerServiceWorker'
 
-const customHistory = createBrowserHistory()
-customHistory.listen(() => window.tracker.pageView())
-
 ReactDOM.render(
-  <Router history={customHistory}>
+  <Router>
     <Switch>
       <Route path="/login/callback" component={LoginCallback} />
       <PrivateRoute path="/" component={App} />
