fb9d10668b6db3256144fb118e5aec09174d34be
Bruno Guedão <brunoguedao@edumobi.com.br>
2019-08-09 16:08:14 -0300

    utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-export { default as SomosClient } from './client.js'
+export { default as SpotifyClient } from './client.js'
 export { getOauthClient } from './oauth'
 export { getToken, setToken, clearToken } from './token'
