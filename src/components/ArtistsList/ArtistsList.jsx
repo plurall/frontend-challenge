67aedb10591ee8b759da6bf8ce83b43662130ee1
Samuel Ramos <samuelramox@outlook.com>
2020-11-11 09:18:34 -0300

    feat: add ArtistsList component

---
+++ src/components/ArtistsList/ArtistsList.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import { Tooltip } from 'plurall-ui'
+
+import styles from './ArtistsList.module.css'
+
+class ArtistsList extends React.Component {
+  async onSelect(artistId) {
+    this.props.history.push(`/artist/${artistId}`)
+  }
+
+  render() {
+    const { artists } = this.props
+
+    return (
+      <div className={styles.wrapper}>
+        {!!artists.length &&
+          artists.map(artist => (
+            <Tooltip content={artist.name} position="bottom">
+              <div
+                className={styles.artistItem}
+                key={artist.id}
+                onClick={() => this.onSelect(artist.id)}
+              >
+                <div className={styles.imageWrapper}>
+                  {!!artist.images.length && (
+                    <img
+                      src={artist.images[0].url}
+                      className={styles.artistImage}
+                      alt={`Imagem de ${artist.name}`}
+                    />
+                  )}
+                </div>
+              </div>
+            </Tooltip>
+          ))}
+      </div>
+    )
+  }
+}
+
+export default withRouter(ArtistsList)
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
+export { default as ArtistsList } from './ArtistsList'
 export { default as Layout } from './Layout'
 export { default as LinkButton } from './LinkButton'
-export { default as SubHeader } from './SubHeader'
 export { default as PrivateRoute } from './PrivateRoute'
+export { default as SubHeader } from './SubHeader'
