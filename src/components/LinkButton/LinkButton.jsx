d37c62259122a461519f1aff3b0a584c14c732b4
Samuel Ramos <samuelramox@outlook.com>
2020-11-11 04:18:39 -0300

    feat: add LinkButton component

---
+++ src/components/LinkButton/LinkButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import styles from './LinkButton.module.css'
+
+const LinkButton = ({ buttonHref, children }) => (
+  <Link to={buttonHref} className={styles.button}>
+    {children}
+  </Link>
+)
+
+LinkButton.propTypes = {
+  buttonHref: PropTypes.string,
+  children: PropTypes.node,
+}
+
+export default LinkButton
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Layout } from './Layout'
+export { default as LinkButton } from './LinkButton'
 export { default as SubHeader } from './SubHeader'
 export { default as PrivateRoute } from './PrivateRoute'
