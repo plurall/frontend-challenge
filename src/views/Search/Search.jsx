a6f0337b884034dc1201c716dd51b309e23b1fff
Samuel Ramos <samuelramox@outlook.com>
2020-11-11 09:19:46 -0300

    feat: add Search view

---
+++ src/views/Search/Search.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import { Alert, TextBox } from 'plurall-ui'
+
+import { ArtistsList, Layout, SubHeader } from 'components'
+import { SomosClient } from 'utils'
+
+import styles from './Search.module.css'
+
+class Search extends React.Component {
+  state = {
+    artists: [],
+    error: {},
+  }
+
+  client = new SomosClient()
+
+  async onSearch(value) {
+    if (value.length > 4) {
+      const res = await this.client.getArtists(value)
+
+      res && res.artists
+        ? this.setState({ artists: res.artists.items })
+        : this.setState({
+            error: res.error,
+          })
+    }
+  }
+
+  render() {
+    const { artists, error } = this.state
+
+    return (
+      <>
+        <Layout>
+          <SubHeader
+            breadcrumb={[{ text: 'Busca' }]}
+            heading="Somos Front-end Challange"
+            buttonHref="/"
+          />
+          <div className={styles.wrapper}>
+            {error.message && (
+              <Alert name={`Erro ${error.status}`} type="error">
+                {error.message}
+              </Alert>
+            )}
+            <TextBox
+              label="Digite o nome do artista"
+              placeholder="Buscar um artista"
+              onChange={value => this.onSearch(value)}
+            />
+            <ArtistsList artists={artists} loading={false} />
+          </div>
+        </Layout>
+      </>
+    )
+  }
+}
+
+export default withRouter(Search)
