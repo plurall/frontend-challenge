43c380764fae7976d45fe082bbc76d39c2fa3944
Gabriel Gomes <gabrielgomes639@gmail.com>
2020-11-29 22:58:23 -0300

    Fix test

--- src/views/Search/search.test.js
+++ src/views/Search/search.test.js
@@ -1,9 +1,9 @@
 const { isGreaterThanFour } = require('./utils')
 
-test('Deve retornar se texto digitado for maior que 4.', () => {
+test('Deve retornar true se texto digitado for maior que 4.', () => {
   expect(isGreaterThanFour('busca artista')).toBeTruthy()
 })
 
-test('Deve retornar se texto digitado for menor que 4.', () => {
+test('Deve retornar true se texto digitado for menor que 4.', () => {
   expect(isGreaterThanFour('busc')).toBeFalsy()
 })
