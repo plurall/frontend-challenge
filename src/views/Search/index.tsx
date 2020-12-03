024e445f2bbac80f3e5e4b6bb6721be1b5fabbc9
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-03 02:19:25 -0300

    Finish Busca view styles

--- src/views/Search/index.tsx
+++ src/views/Search/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-
+import React, { FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import SubHeader from '../../components/SubHeader';
 
 import { SomosClient } from '../../utils';
 
+import styles from './Search.module.css'
+
 const Search: React.FC = () => {
 
   const client = new SomosClient();
+
+  function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ) {
+    event.preventDefault();
+  }
 
   return (
     <>
@@ -16,7 +25,67 @@ const Search: React.FC = () => {
         breadcrumb={[{ text: 'Home  >  Busca' }]}
         heading="Somos Front-end Challange"
       />
-      <h1>Buscar Artista</h1>
+      <div className={styles.container}>
+        <h1>Buscar Artista</h1>
+
+        <form onSubmit={handleAddRepository}>
+          <input
+            // value={newRepo}
+            // onChange={e => setNewRepo(e.target.value)}
+            placeholder="Digite o nome do artista"
+          />
+          <button type="submit">Pesquisar</button>
+        </form>
+
+        {/* {inputError && <Error>{inputError}</Error>} */}
+
+        <div className={styles.artists}>
+          <Link
+            to="/artista"
+          >
+            {/* <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            /> */}
+            <div>
+              <strong>Artista 1</strong>
+              <p>Artista Muito Bom</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+
+            <Link
+              to="/artista"
+            >
+              {/* <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              /> */}
+              <div>
+                <strong>Artista 2</strong>
+                <p>Artista Mais ou Menos</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          {/* {repositories.map(repository => (
+            <Link
+              key={repository.full_name}
+              to={`/repository/${repository.full_name}`}
+            >
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          ))} */}
+        </div>
+      </div>
+
     </>
   )
 }
