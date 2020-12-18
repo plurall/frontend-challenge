b189c9116d0cfc0f28cc016a0852db3c1649b56d
Bruno Soares <vsoares.bruno@gmail.com>
2020-12-18 17:06:44 -0300

    refactor: improvements on client api

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,49 +2,42 @@ import { clearToken, getToken } from 'utils'
 
 class SomosClient {
   constructor() {
-
+    this.access_token = getToken();
+    this.headers = {
+      method: 'GET',
+      headers: {
+       'Authorization': `Bearer ${this.access_token}`,
+       'Content-Type': 'application/json'
+      }
+    }
   }
 
   onError = error => {
-    clearToken()
   }
 
   async getArtists(artistName) {
     const url = `https://api.spotify.com/v1/search?q=${artistName}&type=artist`;
-    const access_token = getToken();
 
-    try {
-      const response = await fetch(url,
-        {
-          method: 'GET',
-          headers: {
-           'Authorization': `Bearer ${access_token}`,
-           'Content-Type': 'application/json'
-          }
-        }
-      );
+      const response = await fetch(url, this.headers).catch(function(error) {
+        // Error handling here!
+        clearToken()
+    });
 
       const json = await response.json();
       return json.artists;
-
-    } catch (error) {
-      return this.onError()
-    }
   }
 
   async getArtistInfo(artistId) {
     const url = `https://api.spotify.com/v1/artists/${artistId}`;
-    const access_token = getToken();
+    const response = await fetch(url, this.headers);
 
-    const response = await fetch(url,
-      {
-        method: 'GET',
-        headers: {
-         'Authorization': `Bearer ${access_token}`,
-         'Content-Type': 'application/json'
-        }
-      }
-    );
+    const json = await response.json();
+    return json;
+  }
+
+  async getArtistAlbums(artistId) {
+    const url = `https://api.spotify.com/v1/artists/${artistId}/albums?limit=10`;
+    const response = await fetch(url, this.headers);
 
     const json = await response.json();
     return json;
