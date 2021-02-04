a99996bfa8464e092471105fdc377dd34e82f373
Vitor Vicente <vitorvicentecorreia@gmail.com>
2021-02-04 20:25:18 -0300

    Configure eslint and prettier

--- src/components/ArtistIntro/ArtistIntro.jsx
+++ src/components/ArtistIntro/ArtistIntro.jsx
@@ -3,42 +3,37 @@ import React from 'react'
 
 import styles from './ArtistIntro.module.css'
 
-export default function ArtistIntro({ artist }){
-    if(!artist) return (
-        <div className="artist-not-found">
-            O artista não foi encontrado.
-        </div>
-    )
+export default function ArtistIntro({ artist }) {
+  if (!artist)
+    return <div className="artist-not-found">O artista não foi encontrado.</div>
 
-    return(
-        <div className={styles.artistIntro} data-testid="artist-intro">
-            <div className={styles.avatar}>
-                <img 
-                    alt={`Foto do artista ${artist.name}`} 
-                    src={artist.photograph}
-                    className={styles.photograph}
-                />
-            </div>
-            <div className={styles.info}>
-                <span className={styles.name}>
-                    {artist.name}
-                </span>
-                <span className={styles.popularity}>
-                    Popularidade: {artist.popularity}
-                </span>
-                <span className={styles.genres}>
-                    Generos: {artist.genres.join(', ')}
-                </span>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.artistIntro} data-testid="artist-intro">
+      <div className={styles.avatar}>
+        <img
+          alt={`Foto do artista ${artist.name}`}
+          src={artist.photograph}
+          className={styles.photograph}
+        />
+      </div>
+      <div className={styles.info}>
+        <span className={styles.name}>{artist.name}</span>
+        <span className={styles.popularity}>
+          Popularidade: {artist.popularity}
+        </span>
+        <span className={styles.genres}>
+          Generos: {artist.genres.join(', ')}
+        </span>
+      </div>
+    </div>
+  )
 }
 
 ArtistIntro.propTypes = {
-    artist: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        popularity: PropTypes.number.isRequired,
-        photograph: PropTypes.string.isRequired,
-        genres: PropTypes.array.isRequired
-    })
+  artist: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    popularity: PropTypes.number.isRequired,
+    photograph: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+  }),
 }
--- src/components/ArtistIntro/ArtistIntro.test.js
+++ src/components/ArtistIntro/ArtistIntro.test.js
@@ -4,36 +4,35 @@ import { render, screen } from '@testing-library/react'
 import ArtistIntro from './ArtistIntro'
 
 const artistMock = {
-    name: 'Péricles',
-    popularity: 72,
-    photograph: 'https://i.scdn.co/image/a2660d83578f7fe5af4e7c0c58d37468bad6b532',
-    genres: ['pagode', 'pagode novo']
+  name: 'Péricles',
+  popularity: 72,
+  photograph:
+    'https://i.scdn.co/image/a2660d83578f7fe5af4e7c0c58d37468bad6b532',
+  genres: ['pagode', 'pagode novo'],
 }
 
 describe('ArtistIntro.jsx', () => {
-    test(`Given that the component was rendered
+  test(`Given that the component was rendered
     and received the artist prop,
     should show name, popularity, photograph and list of genres of artist.`, () => {
-        const textsOnScreen = [
-            'Péricles',
-            'Popularidade: 72',
-            'Generos: pagode, pagode novo',
-        ]
+    const textsOnScreen = [
+      'Péricles',
+      'Popularidade: 72',
+      'Generos: pagode, pagode novo',
+    ]
 
-        render(<ArtistIntro artist={artistMock} />)
+    render(<ArtistIntro artist={artistMock} />)
 
-        textsOnScreen.forEach(text =>
-            expect(screen.getByText(text)).toBeVisible()
-        )
-        expect(screen.getByAltText('Foto do artista Péricles')).toBeVisible()
-    })
+    textsOnScreen.forEach(text => expect(screen.getByText(text)).toBeVisible())
+    expect(screen.getByAltText('Foto do artista Péricles')).toBeVisible()
+  })
 
-    test(`Given that the component was rendered
+  test(`Given that the component was rendered
     and not received the artist prop,
     should render only a message informing that the artist is not found.`, () => {
-        render(<ArtistIntro />)
+    render(<ArtistIntro />)
 
-        expect(screen.getByText('O artista não foi encontrado.')).toBeVisible()
-        expect(screen.queryByTestId('artist-intro')).not.toBeInTheDocument()
-    })
+    expect(screen.getByText('O artista não foi encontrado.')).toBeVisible()
+    expect(screen.queryByTestId('artist-intro')).not.toBeInTheDocument()
+  })
 })
