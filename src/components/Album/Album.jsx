8894715e63e0c2bd3c43876fcaae4ae00b66947c
Vitor Vicente <vitorvicentecorreia@gmail.com>
2021-02-04 21:31:37 -0300

    Update unit tests and create structure of Album
    Change data prop to album.

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,1 +1,34 @@
-export { default } from './Album'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import styles from './Album.module.css'
+
+export default function Album({ album }) {
+  if (!album) return <></>
+
+  const formattedReleaseDate = React.useMemo(() => {
+    const [year, month, day] = album.release_date.split('-')
+
+    return `${day}/${month}/${year}`
+  }, [album])
+
+  return (
+    <div className={styles.album} data-testid="album">
+      <div className={styles.header}>
+        <img src={album.img} alt={`Capa do album ${album.name}`} />
+      </div>
+      <div className={styles.body}>
+        <span className={styles.name}>{album.name}</span>
+        <span className={styles.releaseDate}>{formattedReleaseDate}</span>
+      </div>
+    </div>
+  )
+}
+
+Album.propTypes = {
+  album: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+  }),
+}
--- src/components/Album/Album.test.js
+++ src/components/Album/Album.test.js
@@ -6,23 +6,23 @@ import Album from './Album'
 const albumMock = {
   name: 'Tô Achando Que É Amor',
   release_date: '2020-12-04',
-  image: 'https://i.scdn.co/image/ab67616d00001e0220d711e24ec947c793012af5',
+  img: 'https://i.scdn.co/image/ab67616d00001e0220d711e24ec947c793012af5',
 }
 
 describe('Album.jsx', () => {
   test(`Given that the component has rendered
-    and received the data prop containing the album data,
-    should render image, name and release date of album.`, () => {
+    and received the album prop,
+    should render img, name and release date of album.`, () => {
     const textsOnScreen = ['Tô Achando Que É Amor', '04/12/2020']
 
-    render(<Album data={albumMock} />)
+    render(<Album album={albumMock} />)
 
     textsOnScreen.forEach(text => expect(screen.getByText(text)).toBeVisible())
     expect(screen.getByAltText('Capa do album Tô Achando Que É Amor'))
   })
 
   test(`Given that the component has rendered
-	and not received the data prop,
+	and not received the album prop,
 	should not render the album container.`, () => {
     render(<Album />)
 
