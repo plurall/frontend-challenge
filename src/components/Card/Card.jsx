8e5edebfda88c685dbd71e81d657c8174bd82cd9
Vitor Vicente <vitorvicentecorreia@gmail.com>
2021-02-05 01:14:49 -0300

    Transform Album component in Card
    The Album component was transformed in Card to be reused in several
    pages.

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import styles from './Card.module.css'
+
+export default function Card({ children, img, alt }) {
+  if (!img && !alt) return <></>
+
+  return (
+    <div className={styles.card} data-testid="card">
+      <div className={styles.header}>
+        <img className={styles.img} src={img} alt={alt} />
+      </div>
+      {children && <div className={styles.body}>{children}</div>}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  img: PropTypes.string,
+  alt: PropTypes.string,
+}
---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Card from './Card'
+
+describe('Album.jsx', () => {
+  test(`Given that the component has rendered
+    and received children and props alt and img,
+    should render this informations`, () => {
+    const singer = 'Péricles'
+    const img =
+      'https://i.scdn.co/image/ab67616d00001e0220d711e24ec947c793012af5'
+    const alt = 'Capa do album Tô Achando Que É Amor'
+
+    render(
+      <Card img={img} alt={alt}>
+        {singer}
+      </Card>,
+    )
+
+    expect(screen.getByAltText(alt)).toBeVisible()
+    expect(screen.getByText(singer)).toBeVisible()
+  })
+
+  test(`Given that the component has rendered
+	and not received the album prop,
+	should not render the album container.`, () => {
+    render(<Card />)
+
+    expect(screen.queryByTestId('card')).not.toBeInTheDocument()
+  })
+})
