a0066157722e9afb35c0b1400ffcfa2fcb837490
Bill Marques <pnxbill@gmail.com>
2021-05-30 22:30:37 -0300

    Add function to get single artist in utils

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,13 +1,33 @@
+import axios from 'axios';
 import { clearToken, getToken } from 'utils'
 
 class SomosClient {
-  constructor() {}
+  constructor() {
+    this.baseURL = 'https://api.spotify.com/v1';
+  }
 
-  onError = error => {}
+  onError = error => { }
 
-  async getArtists() {
-    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
-    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+  async getArtists(query) {
+    const { data } = await axios.get(`${this.baseURL}/search`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      },
+      params: {
+        q: query,
+        type: 'artist'
+      }
+    });
+    return data;
+  }
+
+  async getArtist(id) {
+    const { data } = await axios.get(`${this.baseURL}/artist/${id}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+    return data;
   }
 }
 
