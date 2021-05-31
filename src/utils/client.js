bafa462113a71c7e46761f25d0a70f87ecfa209b
Bill Marques <pnxbill@gmail.com>
2021-05-30 22:40:05 -0300

    Add albums to single artist return object

--- src/utils/client.js
+++ src/utils/client.js
@@ -22,13 +22,26 @@ class SomosClient {
   }
 
   async getArtist(id) {
-    const { data } = await axios.get(`${this.baseURL}/artist/${id}`, {
+    let { data } = await axios.get(`${this.baseURL}/artists/${id}`, {
       headers: {
         Authorization: `Bearer ${getToken()}`
       }
     });
+    const { data: { items } } = await axios.get(`${this.baseURL}/artists/${id}/albums`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      },
+      params: {
+        limit: 10
+      }
+    });
+
+    // Set albums to the return object
+    data.albums = items;
+
     return data;
   }
+
 }
 
 export default SomosClient
