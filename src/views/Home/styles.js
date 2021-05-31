c9ab374a8ecce86db610c10394125b9b83db01c3
Bill Marques <pnxbill@gmail.com>
2021-05-31 00:12:44 -0300

    Add start button styles

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -1,26 +1,5 @@
 import styled from 'styled-components';
 
-export const StyleHomeWrapper = styled.div`
-  max-width: 1224px;
-  padding: 30px 20px;
-  margin: 0 auto;
-  font: 16px/14px 'Nunito', sans-serif;
-  height: calc(100vh - var(--height-nav-bar) - var(--height-footer) - var(--height-sub-header));
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  h1 {
-    margin: 10px;
-    position: absolute;
-  }
-
-  a {
-    margin: auto;
-  }
-`; 
-
 export const StyleButtonStart = styled.button`
   padding: 20px 40px;
   border-radius: 38px;
