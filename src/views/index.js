4f8efaca80948d7b1dd91f02c9ea61d676d83dab
Mathues fellipi <matheus.fellipi@outlook.com>
2021-06-05 17:04:07 -0300

    adicionando a exportação do view

--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from './Home'
 export { default as LoginCallback } from './LoginCallback'
+export { default as Search } from './Search'
+export { default as InfoArtist } from './InfoArtist'
