fbda3edfda4eba915707d8c9faa624ffb9887a14
Mathues fellipi <matheus.fellipi@outlook.com>
2021-06-05 17:03:13 -0300

    Adicionando a rota de busca

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
 import { SubHeader } from 'components'
 import { SomosClient } from 'utils'
-
+import { Link } from 'react-router-dom'
+import { Button } from 'plurall-ui'
 import styles from './Home.module.css'
 
 class Home extends React.Component {
@@ -18,7 +18,9 @@ class Home extends React.Component {
           heading="Somos Front-end Challange"
         />
         <div className={styles.wrapper}>
-          <h1>Hi</h1>
+          <Link to="/busca">
+            <Button>pagina de buscar</Button>
+          </Link>
         </div>
       </React.Fragment>
     )
