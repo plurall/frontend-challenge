0f560450a9941353f4813100507e07087735d1a1
Mathues fellipi <matheus.fellipi@outlook.com>
2021-06-05 17:02:45 -0300

    adicionando as exportação do componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Layout } from './Layout'
 export { default as SubHeader } from './SubHeader'
 export { default as PrivateRoute } from './PrivateRoute'
+export { default as Results } from './ResultArtits'
+export { default as DetailsArtist } from './DetailsArtist'
+export { default as CardAlbum } from './CardAlbum'
