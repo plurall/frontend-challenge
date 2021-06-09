7b432dc005133f2e7ec2aa6fb8ad03a694a51fe1
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-06-09 07:21:31 -0300

    added imports from tests

---
+++ src/tests/index.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom'
+import { screen, render, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+export { screen, render, within, userEvent }
