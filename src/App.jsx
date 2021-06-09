abd476f393fc62f6fad28f5dc6a894c3bcc72ac0
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-06-09 07:28:51 -0300

    added new private routes to search and artist

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
-import { Layout } from 'components'
-import { Home } from 'views'
+import { Layout, PrivateRoute } from 'components'
+import { Artist, Home, Search } from 'views'
 
 import './App.module.css'
 
 const App = () => (
-  <Layout>
-    <Home />
-  </Layout>
+  <BrowserRouter>
+    <Switch>
+      <Layout>
+        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute path="/search" component={Search} />
+        <PrivateRoute path="/artist/:id" component={Artist} />
+      </Layout>
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
