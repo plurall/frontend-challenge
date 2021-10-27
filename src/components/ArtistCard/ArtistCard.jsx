4d91fa9173ffdc728639ba73e0736fd60a2d15fb
Lucas Nakahara <nakahwra@gmail.com>
2021-10-26 23:44:05 -0300

    ✨ Render fetched artists

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styles from './ArtistCard.module.css';
 
-const ArtistCard = () => {
+const ArtistCard = ({ artist }) => {
+  console.log(artist.image)
   return (
-    <div className={ styles.card }>
+    <div className={` ${styles.card} animate__animated animate__fadeIn`}>
       <div className={ styles.imgContainer}>
-        <img src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="person" />
+        <img src={artist.image} alt={`${artist.name} profile image`} />
       </div>
-      <span>Artist Name</span>
+      <span>{ artist.name }</span>
     </div>
   )
 }
--- src/views/Search/Search.jsx
+++ src/views/Search/Search.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ArtistCard, Layout } from 'components';
 import styles from './Search.module.css';
+import { SomosClient } from 'utils';
 
-const Search = () => (
-  <Layout>
-    <div className={ styles.container }>
-      <h1>Pesquise seu artista</h1>
-      <input type="text" />
+const Search = () => {
+  const [input, setInput] = useState('');
+  const [artists, setArtists] = useState([])
+  
+  const client = new SomosClient();
 
-      <div className={ styles.artistGrid }>
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
+  useEffect(() => {
+    console.log(artists);
+  }, [artists])
+
+  async function handleGetArtists(query, limit = 12) {
+    const response = await client.getArtists(query, limit);
+
+    const newArtists = response.artists.items.map((artist) => {
+      const artistImage = artist.images[0]
+      return {
+        id: artist.id,
+        name: artist.name,
+        image: artistImage ? artistImage.url : '',
+      }
+    })
+    return newArtists
+  }
+  
+  async function handleInput(event) {
+    const newInput = event.target.value.toString()
+    if (newInput.length > 3) {
+      setArtists(await handleGetArtists(newInput))
+    }
+  }
+
+  return (
+    <Layout>
+      <div className={ styles.container }>
+        <div className="animate__animated animate__fadeInDown" >
+          <h1>Pesquise seu artista</h1>
+          <input type="text" onChange={handleInput}/>
+        </div>
+
+        <div className={ styles.artistGrid }>
+          {
+            artists.map(artist => (
+              <ArtistCard key={artist.id} artist={artist} />
+            ))
+          }
+        </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default Search;
