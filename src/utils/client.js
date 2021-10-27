28454e0d02649f2a4d705e93fb941889fda0d146
Lucas Nakahara <nakahwra@gmail.com>
2021-10-27 02:55:48 -0300

    ♻️ Refactor api query method

--- src/utils/client.js
+++ src/utils/client.js
@@ -5,13 +5,11 @@ class SomosClient {
 
   onError = error => {}
 
-  async getArtists(query, limit) {
-    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
-    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+  async apiQuery(query) {
     try {
       const token = getToken();
 
-      const response = await fetch(encodeURI(`https://api.spotify.com/v1/search?q=${query}&type=artist&limit=${limit}`), {
+      const response = await fetch(encodeURI(query), {
         headers: {
           Authorization: `Bearer ${token}`,
           'Accept': 'application/json',
@@ -19,11 +17,25 @@ class SomosClient {
         }
       });
 
-      return await response.json()
+      return response.json()
+
     } catch (err) {
       console.log(err)
     }
   }
+
+  getArtists(query, limit) {
+    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
+    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=artist&limit=${limit}`
+    return this.apiQuery(url)
+  }
+
+  getArtist(id) {
+    const url = `https://api.spotify.com/v1/artists/${id}`
+    return this.apiQuery(url)
+  }
+  
 }
 
 export default SomosClient
