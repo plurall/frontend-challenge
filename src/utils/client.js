15ecdf0f0d18faf460fb03d5530a9ec298833068
Lucas Nakahara <nakahwra@gmail.com>
2021-10-26 23:43:27 -0300

    ✨ Fetch artists by query from Spotify API

--- src/utils/client.js
+++ src/utils/client.js
@@ -5,9 +5,24 @@ class SomosClient {
 
   onError = error => {}
 
-  async getArtists() {
+  async getArtists(query, limit) {
     // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
     // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+    try {
+      const token = getToken();
+
+      const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=artist&limit=${limit}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      });
+
+      return await response.json()
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
