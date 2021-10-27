c9accbbf5acf4d4a757f8abcd5a0b77c4dacee66
Lucas Nakahara <nakahwra@gmail.com>
2021-10-27 00:55:40 -0300

    💫 Update card animation

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -5,7 +5,7 @@ import styles from './ArtistCard.module.css';
 const ArtistCard = ({ artist }) => {
   console.log(artist.image)
   return (
-    <div className={` ${styles.card} animate__animated animate__fadeIn`}>
+    <div className={` ${styles.card} animate__animated animate__fadeInUp`}>
       <div className={ styles.imgContainer}>
         <img src={artist.image} alt={`${artist.name} profile image`} />
       </div>
