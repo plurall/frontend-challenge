af32d25bafbda89b69be1dd96876fb38e04a086a
William Braz <william.braz@somoseducacao.com.br>
2022-04-06 11:31:47 -0300

    Update docs

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -15,7 +15,7 @@ class Home extends React.Component {
       <React.Fragment>
         <SubHeader
           breadcrumb={[{ text: 'Home' }]}
-          heading="Somos Front-end Challange"
+          heading="Desafio Front-end do Plurall"
         />
         <div className={styles.wrapper}>
           <h1>Hi</h1>
