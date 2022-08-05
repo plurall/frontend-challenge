3d38ddd673329362e4c99792523c082d1014a50b
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-08-04 21:27:41 -0300

    feat: Realiza chamadas para a API do Spotify

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,16 +2,29 @@
 import { clearToken, getToken } from 'utils'
 
 class SomosClient {
-  // eslint-disable-next-line
-  constructor() {}
+  constructor(value) {
+    this.value = value
+  }
 
   // eslint-disable-next-line
   onError = error => {}
 
-  // eslint-disable-next-line
   async getArtists() {
-    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
-    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+    const request = await fetch(`https://api.spotify.com/v1/search?q=${this.value}&type=artist`, { headers: { Authorization: `Bearer ${getToken()}` } })
+    const requestJson = await request.json()
+    return requestJson
+  }
+
+  async getArtistById() {
+    const request = await fetch(`https://api.spotify.com/v1/artists/${this.value}`, { headers: { Authorization: `Bearer ${getToken()}` } })
+    const requestJson = await request.json()
+    return requestJson
+  }
+
+  async getAlbums() {
+    const request = await fetch(`https://api.spotify.com/v1/artists/${this.value}/albums?offset=0&limit=10`, { headers: { Authorization: `Bearer ${getToken()}` } })
+    const requestJson = await request.json()
+    return requestJson
   }
 }
 
