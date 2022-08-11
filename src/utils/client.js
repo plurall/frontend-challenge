954a58d40c3ccf3bcb62369daf6a3faff3d4c306
Vinicius <vinicunha2008@gmail.com>
2022-08-10 22:23:06 -0300

    api calls

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
 import { clearToken, getToken } from 'utils'
+import axios from 'axios'
 
 class SomosClient {
   // eslint-disable-next-line
@@ -9,9 +10,63 @@ class SomosClient {
   onError = error => {}
 
   // eslint-disable-next-line
-  async getArtists() {
-    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
-    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+  async getIdsByName(name) {
+    try {
+      const artistsByName = await axios.get(`https://api.spotify.com/v1/search?q=artist%3A${name}&type=artist`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+
+      return artistsByName.data.artists.items.reduce((acc, curr, index) => index === 0 ? `${acc}${curr.id}` : `${acc},${curr.id}`, '')
+    } catch (error) {
+      clearToken()
+    }
+  }
+  // eslint-disable-next-line
+  async getArtistsByName(name) {
+    try {
+      const idList = await this.getIdsByName(name)
+
+      const artists = await axios.get(`https://api.spotify.com/v1/artists?ids=${idList}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+
+      return artists
+    } catch (error) {
+      clearToken()
+    }
+  }
+  // eslint-disable-next-line
+  async getArtistById(id) {
+    try {
+      // get albums `https://api.spotify.com/v1/artists/${id}/albums`
+      const artist = await axios.get(`https://api.spotify.com/v1/artists/${id}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+
+      return artist
+    } catch (error) {
+      clearToken()
+    }
+  }
+  // eslint-disable-next-line
+  async getAlbumsByArtistId(artistId) {
+    try {
+      const albums = await axios.get(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=10`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+
+      return albums
+    } catch (error) {
+      clearToken()
+    }
   }
 }
 
