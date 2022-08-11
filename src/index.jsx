f8ca0ead2b78d33b52d4f3b2c992589f27ab9aea
Vinicius <vinicunha2008@gmail.com>
2022-08-10 22:21:46 -0300

    routes creation

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { LoginCallback } from 'views'
+import { Artist, LoginCallback, Search } from 'views'
 import { PrivateRoute } from 'components'
 import App from 'App'
 import registerServiceWorker from 'registerServiceWorker'
@@ -10,7 +10,9 @@ import registerServiceWorker from 'registerServiceWorker'
 ReactDOM.render(
   <Router>
     <Switch>
+      <PrivateRoute path="/artista/:id" component={Artist} />
       <Route path="/login/callback" component={LoginCallback} />
+      <PrivateRoute path="/busca" component={Search} />
       <PrivateRoute path="/" component={App} />
     </Switch>
   </Router>,
