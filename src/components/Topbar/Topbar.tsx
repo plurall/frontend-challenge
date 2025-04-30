a1e2cb9c78fcfab6731820e00dcf8b1a5bab032c
Franklin Sales <franklin.salesx@gmail.com>
2025-04-30 13:03:24 -0300

    feat: adiciona logo da Plurall

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Topbar.scss';
+import PlurallBrandIcon from '../icons/PlurallBrandIcon';
 
 const Topbar: React.FC = () => {
   return (
     <div className="topbar">
       <div className="topbar-container">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          <PlurallBrandIcon width="100" height="32" fill="#ffffff" />
+        </div>
         <nav className="menu">
           <ul>
             <li><a href="#home">Home</a></li>
