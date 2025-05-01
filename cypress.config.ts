746ad7f3f75758fa23b9185159a0bcc5838f4b4d
Franklin Sales <franklin.salesx@gmail.com>
2025-05-01 15:19:31 -0300

    feat: instala e configura o cypress para testes

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
