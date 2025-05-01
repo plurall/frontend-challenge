cd6d9490ec7e189de593356928e5501fe60bb433
Franklin Sales <franklin.salesx@gmail.com>
2025-05-01 17:46:23 -0300

    feat: atualiza testes da home logado e não logado

--- src/components/Home/LoggedIn/LoggedIn.tsx
+++ src/components/Home/LoggedIn/LoggedIn.tsx
@@ -5,8 +5,20 @@ import './LoggedIn.scss';
 const LoggedIn: React.FC = () => {
   return (
     <div className="logged-in">
-      <h1 className="logged-in-title">Buscar Artista</h1>
-      <Link to="/busca" className="logged-in-button">
+      <h1 
+        className="logged-in-title"
+        data-testid="logged-in-title"
+        data-cy="logged-in-title"
+      >
+          Buscar Artista
+      </h1>
+      <Link 
+        to="/busca" 
+        className="logged-in-button"
+        data-testid="logged-in-button"
+        data-cy="logged-in-button"
+        aria-label="Ir para Busca"
+      >
         Ir para Busca
       </Link>
     </div>
