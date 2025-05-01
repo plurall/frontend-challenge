b6e3b376c224d0ee4938f0b85e39fba06b132eaf
Franklin Sales <franklin.salesx@gmail.com>
2025-05-01 13:32:00 -0300

    fix: formatação do código

--- src/components/Search/Searchbar/Searchbar.tsx
+++ src/components/Search/Searchbar/Searchbar.tsx
@@ -11,13 +11,13 @@ interface IProps {
 const Searchbar = ({ setArtists }: IProps) => {
   const navigate = useNavigate();
   const { token } = useSpotifyToken();
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const queryParam = urlParams.get('query');
+    const queryParam = urlParams.get("query");
     if (queryParam) {
       setQuery(queryParam);
     }
@@ -30,58 +30,57 @@ const Searchbar = ({ setArtists }: IProps) => {
           setIsSearching(true);
           const results = await searchArtists(query, token);
           setArtists(results);
-            // update url with query without reloading the page
-            navigate(`?query=${encodeURIComponent(query)}`, { replace: true });
+          // update url with query without reloading the page
+          navigate(`?query=${encodeURIComponent(query)}`, { replace: true });
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
           if (error.response?.status === 401) {
-            console.error('Token inválido ou expirado. Atualize o token.');
+            console.error("Token inválido ou expirado. Atualize o token.");
           } else {
-            console.error('Erro ao buscar artistas:', error);
+            console.error("Erro ao buscar artistas:", error);
           }
         }
       } else {
         setArtists([]);
       }
     } catch (error) {
-      console.error('Erro ao buscar artistas:', error);
+      console.error("Erro ao buscar artistas:", error);
     } finally {
       setIsSearching(false);
     }
   };
 
   useEffect(() => {
-    if(!token) return;
-    if(query.length >= 4) {
+    if (!token) return;
+    if (query.length >= 4) {
       const timeoutId = setTimeout(() => {
         fetchArtists();
       }, 500); // Delay de 500ms
 
       return () => clearTimeout(timeoutId);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query, token]);
 
-
-  return (<div className="search-bar">
-          <input
-            type="text"
-            placeholder="Busque por artistas"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="search-input"
-          />
-          <button 
-            onClick={fetchArtists} 
-            disabled={isSearching}
-            className="search-button" 
-          >
-  
-            {isSearching ? '...' : 'Buscar'}
-  
-          </button>
-        </div>)
-}
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Busque por artistas"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="search-input"
+      />
+      <button
+        onClick={fetchArtists}
+        disabled={isSearching}
+        className="search-button"
+      >
+        {isSearching ? "..." : "Buscar"}
+      </button>
+    </div>
+  );
+};
 
 export default Searchbar;
