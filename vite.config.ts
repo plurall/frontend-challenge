43d20792efafbc8ab911cf206c92fbb9e9b67112
Franklin Sales <franklin.salesx@gmail.com>
2025-05-01 18:21:09 -0300

    fix: corrige redirecionamento do login do spotify para o vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173
+  }
 })
