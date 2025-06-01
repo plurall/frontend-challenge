936855386ed23ff8b6d877c0e58f7e1c41bdb268
itslucassouza <lucasvini193@hotmail.com>
2025-06-01 17:36:43 -0300

    fix: fix service and imports

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,4 @@
 export { default as Home } from './Home'
 export { default as Error } from './Error/Error'
-export { default as Search } from './Search/Search'
+export { default as Search } from './SearchArtist'
 export { default as LoginCallback } from './LoginCallback'
