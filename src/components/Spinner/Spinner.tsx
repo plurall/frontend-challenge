b1af3eb046f9f0ae142377224d4642f658ceaa67
itslucassouza <lucasvini193@hotmail.com>
2025-06-01 17:38:11 -0300

    feat: add spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as styles from './Spinner.module.scss'
+
+const Spinner = () => {
+  return <div className='spinner' aria-label='Loading...' />
+}
+
+export default Spinner
