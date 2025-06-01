fdabeb45fed1a3e31409d3ed74766afcd96d9c4d
itslucassouza <lucasvini193@hotmail.com>
2025-06-01 12:11:04 -0300

    fix: private pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Layout } from 'components'
-import { Home } from 'views'
 
 import './App.module.scss'
+import { Outlet } from 'react-router-dom'
 
 const App = () => (
   <Layout>
-    <Home />
+    <Outlet />
   </Layout>
 )
 
